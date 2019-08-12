da50350ef552f4b296f7faab050c0e5682217382
Dayo <Calikidd86@gmail.com>
2019-08-12 14:27:11 -0700

    Tested out packaging application. Modified CharacterSheetEditor into a pdf file editor.

--- src/main/java/dragonsandflagons/charactergenerator/CharacterSheetEditor.java
+++ src/main/java/dragonsandflagons/charactergenerator/CharacterSheetEditor.java
@@ -7,115 +7,223 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CharacterSheetEditor {
 
-    private static final File CHARACTER_SHEET_ALTERNATIVE_PDF = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/CharacterSheetAlternative.pdf")
-            .getFile());
+  //    private static final File CHARACTER_SHEET_ALTERNATIVE_PDF = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/CharacterSheetAlternative.pdf")
+  //            .getFile());
+  //
+  //    private static final File CHARACTER_SHEET_COMPLETE = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/CharacterSheetComplete.pdf")
+  //            .getFile());
+  //
+  //    private static final File CHARACTER_DETAIL_FORM = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/CharacterDetailsForm.pdf")
+  //            .getFile());
+  //
+  //    private static final File TYRANNY_OF_DRAGONS_SHEET = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/TyrannyOfDragons.pdf")
+  //            .getFile());
+  //
+  //    private static final File SPELLCASTING_SHEET = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/SpellcastingSheet.pdf")
+  //            .getFile());
+  //
+  //    private static final File CHARACTER_SHEET_FIFTH_EDITION = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/CharacterSheet5E.pdf")
+  //            .getFile());
+  //
+  //    private static final File DND_CHARACTER_SHEET = new
+  // File(CharacterSheetEditor.class.getClassLoader()
+  //            .getResource("CharacterSheetPDFs/DnD/CharacterSheet.pdf")
+  //            .getFile());
 
-    private static final File CHARACTER_SHEET_COMPLETE = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/CharacterSheetComplete.pdf")
-            .getFile());
+  /**
+   * Create a fillable form copy of the provided pdf document.
+   *
+   * @param document The original pdf document to be copied.
+   * @param copyFileName The name for the new copy of the file.
+   * @return A fillable form copy of the provided pdf document.
+   * @throws IOException If original document not found.
+   */
+  public static PDDocument createCopyOfPDF(PDDocument document, String copyFileName) throws IOException {
+    // Create an extra document to hold the future copy
+    PDDocument newSheet = new PDDocument();
 
-    private static final File CHARACTER_DETAIL_FORM = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/CharacterDetailsForm.pdf")
-            .getFile());
+    // Create a list of the fields and an int to iterate when we make updates
+    List<PDField> fields = new ArrayList<>();
+    int i = 0;
 
-    private static final File TYRANNY_OF_DRAGONS_SHEET = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/TyrannyOfDragons.pdf")
-            .getFile());
+    // Grab the Fillable Fields from the PDF
+    PDDocumentCatalog docCatalog = document.getDocumentCatalog();
+    PDAcroForm pdAcroForm = docCatalog.getAcroForm();
 
-    private static final File SPELLCASTING_SHEET = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/SpellcastingSheet.pdf")
-            .getFile());
-
-    private static final File CHARACTER_SHEET_FIFTH_EDITION = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/CharacterSheet5E.pdf")
-            .getFile());
-
-    private static final File DND_CHARACTER_SHEET = new File(CharacterSheetEditor.class.getClassLoader()
-            .getResource("CharacterSheetPDFs/DnD/CharacterSheet.pdf")
-            .getFile());
-
-    /**
-     * Create a fillable form copy of the provided pdf document.
-     * @param document The original pdf document to be copied.
-     * @return A fillable form copy of the provided pdf document.
-     * @throws IOException If original document not found.
-     */
-    public static PDDocument createCopyOfPDF(PDDocument document) throws IOException {
-        // Create an extra document to hold the future copy
-        PDDocument newSheet = new PDDocument();
-
-        // Create a list of the fields and an int to iterate when we make updates
-        List<PDField> fields = new ArrayList<>();
-        int i = 0;
-
-        // Grab the Fillable Fields from the PDF
-        PDDocumentCatalog docCatalog = document.getDocumentCatalog();
-        PDAcroForm pdAcroForm = docCatalog.getAcroForm();
-
-        for (PDPage page: document.getPages()){
-            for (PDField field : pdAcroForm.getFields()) {
-                // get the fields that are check boxes.
-                fields.add(field);
-            }
-            newSheet.addPage(page);
+    for (PDPage page : document.getPages()) {
+      if (pdAcroForm != null) {
+        for (PDField field : pdAcroForm.getFields()) {
+          // get the fields that are check boxes.
+          fields.add(field);
         }
-
-        // Create new PDAcroForm to apply to our new copy
-        PDAcroForm newForm = new PDAcroForm(newSheet);
-        newSheet.getDocumentCatalog().setAcroForm(newForm);
-        newForm.setFields(fields);
-
-        // Save and close new document
-        return newSheet;
+      }
+      newSheet.addPage(page);
     }
 
-    /**
-     * Fill out and save a copy of the character sheet document provided.
-     * @param characterSheet the original character sheet pdf document.
-     * @throws IOException If original document not found.
-     */
-    public static void fillOutCharacterSheet(PDDocument characterSheet) throws IOException {
-        PDDocument characterSheetCopy = createCopyOfPDF(characterSheet);
+    // Create new PDAcroForm to apply to our new copy
+    PDAcroForm newForm = new PDAcroForm(newSheet);
+    newSheet.getDocumentCatalog().setAcroForm(newForm);
+    newForm.setFields(fields);
 
-        // Grab the Fillable Fields from the PDF
-        PDDocumentCatalog docCatalog = characterSheetCopy.getDocumentCatalog();
-        PDAcroForm pdAcroForm = docCatalog.getAcroForm();
+    // Save and close new document
+    newSheet.save(copyFileName + ".pdf");
+    return newSheet;
+  }
 
-        for (PDPage page: characterSheetCopy.getPages()){
-            for (PDField field : pdAcroForm.getFields()) {
+  /**
+   * Create a fillable form copy of the provided pdf document.
+   *
+   * @param document The original pdf document to be copied.
+   * @return A fillable form copy of the provided pdf document.
+   * @throws IOException If original document not found.
+   */
+  public static PDDocument createCopyOfPDF(PDDocument document) throws IOException {
+    // Create an extra document to hold the future copy
+    PDDocument newSheet = new PDDocument();
 
-                PDField fieldCopy = field;
-                // get the fields that are check boxes.
-                if (!field.getFieldType().equalsIgnoreCase("btn")) {
-                    field.setValue(JOptionPane.showInputDialog("Enter value for: "
-                            + field.getPartialName()));
-                    field.setPartialName(field.getPartialName());
-                }
-            }
+    // Create a list of the fields and an int to iterate when we make updates
+    List<PDField> fields = new ArrayList<>();
+    int i = 0;
+
+    // Grab the Fillable Fields from the PDF
+    PDDocumentCatalog docCatalog = document.getDocumentCatalog();
+    PDAcroForm pdAcroForm = docCatalog.getAcroForm();
+
+    for (PDPage page : document.getPages()) {
+      if (pdAcroForm != null) {
+        for (PDField field : pdAcroForm.getFields()) {
+          // get the fields that are check boxes.
+          fields.add(field);
         }
-
-        characterSheetCopy.save("NewSheetFilled.pdf");
-        characterSheetCopy.close();
+      }
+      newSheet.addPage(page);
     }
 
-    public static void main(String[] args) throws IOException {
+    // Create new PDAcroForm to apply to our new copy
+    PDAcroForm newForm = new PDAcroForm(newSheet);
+    newSheet.getDocumentCatalog().setAcroForm(newForm);
+    newForm.setFields(fields);
 
-        PDDocument document = PDDocument.load(CHARACTER_SHEET_ALTERNATIVE_PDF);
+    // Save and close new document
+    newSheet.save("GenericSheet.pdf");
+    return newSheet;
+  }
 
-        try {
-            fillOutCharacterSheet(document);
-        } catch (IOException e) {
-            System.out.println("File not found at " + e);
+  /**
+   * Fill out and save a copy of the pdf document provided.
+   *
+   * @param pdDocument the original character sheet pdf document.
+   * @throws IOException If original pdf file not found.
+   */
+  public static void fillOutPDFFile(PDDocument pdDocument) throws IOException {
+    PDDocument characterSheetCopy = createCopyOfPDF(pdDocument);
+
+    // Grab the Fillable Fields from the PDF
+    PDDocumentCatalog docCatalog = characterSheetCopy.getDocumentCatalog();
+    PDAcroForm pdAcroForm = docCatalog.getAcroForm();
+
+    for (PDPage page : characterSheetCopy.getPages()) {
+      for (PDField field : pdAcroForm.getFields()) {
+
+        PDField fieldCopy = field;
+        // get the fields that are check boxes.
+        if (!field.getFieldType().equalsIgnoreCase("btn")) {
+          field.setValue(JOptionPane.showInputDialog("Enter value for: " + field.getPartialName()));
+          field.setPartialName(field.getPartialName());
         }
-
+      }
     }
 
+    characterSheetCopy.save("GenericFilledSheet.pdf");
+    characterSheetCopy.close();
+  }
+
+  /**
+   * Creates and stores a copy of a fillable pdf file, offering user input to complete form
+   * during processing.
+   *
+   * @param pdDocument The original PDF document to be filled out.
+   * @param copyFileName The name for the new copy of the file.
+   * @throws IOException If file not found.
+   */
+  public static void fillOutPDFFile(PDDocument pdDocument, String copyFileName) throws IOException {
+    PDDocument characterSheetCopy = createCopyOfPDF(pdDocument);
+
+    // Grab the Fillable Fields from the PDF
+    PDDocumentCatalog docCatalog = characterSheetCopy.getDocumentCatalog();
+    PDAcroForm pdAcroForm = docCatalog.getAcroForm();
+
+    for (PDPage page : characterSheetCopy.getPages()) {
+      for (PDField field : pdAcroForm.getFields()) {
+
+        PDField fieldCopy = field;
+        // get the fields that are check boxes.
+        if (!field.getFieldType().equalsIgnoreCase("btn")) {
+          field.setValue(JOptionPane.showInputDialog("Enter value for: " + field.getPartialName()));
+          field.setPartialName(field.getPartialName());
+        }
+      }
+    }
+
+    characterSheetCopy.save(copyFileName + ".pdf");
+    characterSheetCopy.close();
+  }
+
+  /**
+   * JFileChooser that only allows the selection of .pdf files
+   * @return PDF format File object.
+   */
+  public static File selectPDFFile() {
+    // Configure File chooser to select character sheet pdf
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setDialogTitle("Select character sheet pdf");
+    fileChooser.setAcceptAllFileFilterUsed(false);
+    FileNameExtensionFilter filter =
+        new FileNameExtensionFilter("Character sheet in pdf format", "pdf");
+    fileChooser.addChoosableFileFilter(filter);
+
+    File pdfFile = null;
+
+    int fileChooserResponse = fileChooser.showOpenDialog(null);
+
+    if (fileChooserResponse == JFileChooser.APPROVE_OPTION) {
+      pdfFile = fileChooser.getSelectedFile();
+    }
+
+    return pdfFile;
+  }
+
+  public static void main(String[] args) throws IOException {
+
+    File pdfFile = selectPDFFile();
+
+    try {
+      PDDocument document = PDDocument.load(pdfFile);
+      fillOutPDFFile(document);
+    } catch (IOException e) {
+      System.out.println("File not found at " + e);
+    } catch (NullPointerException n) {
+      System.out.println("Missing required file. ");
+      n.printStackTrace();
+    }
+  }
 }
